16fd20accf4051e57a89d2c967e03dcf16aa32a1
Henri Gabriel <henri@workforpizza.com>
2014-08-11 14:07:50 -0700

    Add Wand.

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from wand.image import Image
+from wand.display import display
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return 'Iconer!'
+
+@app.route('/wand')
+def wand():
+  with Image(filename='image.png') as img:
+    for r in 1, 2, 3:
+      with img.clone() as i:
+        i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+        i.rotate(90 * r)
+        i.save(filename='image-{0}.png'.format(r))
 
 if __name__ == '__main__':
   app.run()
