275fa6ede5f6654fb18891cbb10f391adfa9e769
Henri Gabriel <henri@workforpizza.com>
2014-08-12 10:48:24 -0700

    Add picopt.

--- app.py
+++ app.py
@@ -35,6 +35,15 @@ def receive():
   images.save(i, None, iname)
   return f.redirect(f.url_for('index'))
 
+@app.route('/wand')
+def wand():
+  from wand.image import Image
+  with Image(filename='image.png') as img:
+    for r in 1, 2, 3:
+      with img.clone() as i:
+        i.resize(int(i.width * r * 0.25), int(i.height * r * 0.25))
+        i.rotate(90 * r)
+        i.save(filename='image{0}.png'.format(r))
 
 if __name__ == '__main__':
   app.run()
